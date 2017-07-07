281e00b24be57f620c4f9e7729181e97fdd0f902
davlatbek <idavlatbek19@gmail.com>
2017-07-07 19:27:04 +0300

    add login, register and user  activities

---
+++ innopass/src/main/java/innopolis/innopass/activities/AdminProfileActivity.java
@@ -0,0 +1,15 @@
+package innopolis.innopass.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import innopolis.innopass.R;
+
+public class AdminProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_profile);
+    }
+}
