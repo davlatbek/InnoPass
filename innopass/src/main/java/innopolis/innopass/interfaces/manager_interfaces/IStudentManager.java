14282f53e26f9e86d95e7772f017e0b92d5e43e0
davlatbek <idavlatbek19@gmail.com>
2017-07-09 19:24:45 +0300

    add student manager interface

--- innopass/src/main/java/innopolis/innopass/interfaces/manager_interfaces/IStudentManager.java
+++ innopass/src/main/java/innopolis/innopass/interfaces/manager_interfaces/IStudentManager.java
@@ -1,8 +1,15 @@
 package innopolis.innopass.interfaces.manager_interfaces;
 
+import java.util.List;
+
+import innopolis.innopass.models.Student;
+
 /**
  * Created by davlet on 7/9/17.
  */
 
-interface IStudentManager {
+public interface IStudentManager {
+    List<Student> getAllStudentsList();
+    List<Student> getStudentsListByGroupId(Long groupId);
+    Student getStudentById(Long studentId);
 }
