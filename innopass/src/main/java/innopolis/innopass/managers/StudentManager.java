ecc69d11c981bf659192b35aeb51be2668d2ebca
davlatbek <idavlatbek19@gmail.com>
2017-07-09 19:28:30 +0300

    implement student manager interface methods

--- innopass/src/main/java/innopolis/innopass/managers/StudentManager.java
+++ innopass/src/main/java/innopolis/innopass/managers/StudentManager.java
@@ -1,8 +1,27 @@
 package innopolis.innopass.managers;
 
+import java.util.List;
+
+import innopolis.innopass.interfaces.manager_interfaces.IStudentManager;
+import innopolis.innopass.models.Student;
+
 /**
  * Created by davlet on 7/9/17.
  */
 
-public class StudentManager {
+public class StudentManager implements IStudentManager {
+    @Override
+    public List<Student> getAllStudentsList() {
+        return null;
+    }
+
+    @Override
+    public List<Student> getStudentsListByGroupId(Long groupId) {
+        return null;
+    }
+
+    @Override
+    public Student getStudentById(Long studentId) {
+        return null;
+    }
 }
