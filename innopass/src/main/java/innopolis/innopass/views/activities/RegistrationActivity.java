4d7dba8b6f139ffb04d828ed74a5e149972fe407
davlatbek <idavlatbek19@gmail.com>
2017-07-10 16:06:59 +0300

    add button onclick methods

--- innopass/src/main/java/innopolis/innopass/views/activities/RegistrationActivity.java
+++ innopass/src/main/java/innopolis/innopass/views/activities/RegistrationActivity.java
@@ -8,12 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import innopolis.innopass.R;
 import innopolis.innopass.database.DatabaseHelper;
 import innopolis.innopass.interfaces.view_interfaces.IRegistrationView;
+import innopolis.innopass.models.User;
 import innopolis.innopass.presenters.RegistrationPresenter;
 
 /**
@@ -21,11 +26,15 @@ import innopolis.innopass.presenters.RegistrationPresenter;
  */
 
 public class RegistrationActivity extends AppCompatActivity implements IRegistrationView {
-    @BindView(R.id.editTextLog) EditText editTextLogin;
-    @BindView(R.id.editTextPass) EditText editTextPass;
-    @BindView(R.id.editTextPasswordConfirm) EditText editTextPassConfirm;
-    @BindView(R.id.buttonRegistration) Button buttonRegister;
-    @BindView(R.id.buttonCancelRegistration) Button buttonCancel;
+    @BindView(R.id.editRegLogin) EditText editTextLogin;
+    @BindView(R.id.editRegPass) EditText editTextPass;
+    @BindView(R.id.editRegPasswordConfirm) EditText editTextPassConfirm;
+    @BindView(R.id.buttonRegRegister) Button buttonRegister;
+    @BindView(R.id.buttonRegCancel) Button buttonCancel;
+    @BindView(R.id.editRegFirstName) EditText editFirstName;
+    @BindView(R.id.editRegSurname) EditText editSurname;
+    @BindView(R.id.editRegMiddleName) EditText editRegMiddleName;
+    @BindView(R.id.editRegDob) EditText editRegDateOfBirth;
     private Context context;
     private RegistrationPresenter presenter;
 
@@ -34,18 +43,32 @@ public class RegistrationActivity extends AppCompatActivity implements IRegistra
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         context = this;
+        ButterKnife.bind(this);
         presenter = RegistrationPresenter.getInstance(this, DatabaseHelper.getInstance(this));
     }
 
-    @OnClick(R.id.buttonRegistration)
+    @OnClick(R.id.buttonRegRegister)
     @Override
     public void registerUserButton(View view) {
-
+        presenter.registerNewUser(new User(editTextLogin.getText().toString(),
+                editTextPass.getText().toString(), editFirstName.getText().toString(),
+                editSurname.getText().toString(), editRegMiddleName.getText().toString(),
+                Calendar.getInstance(), R.drawable.lillie));
     }
 
-    @OnClick(R.id.buttonCancelRegistration)
+    @OnClick(R.id.buttonRegCancel)
     @Override
-    public void cancelRegistration() {
+    public void cancelRegistration(View view) {
         startActivity(new Intent(context, LoginActivity.class));
+    }
+
+    @Override
+    public void goToLoginScreen() {
+        startActivity(new Intent(context, LoginActivity.class));
+    }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
