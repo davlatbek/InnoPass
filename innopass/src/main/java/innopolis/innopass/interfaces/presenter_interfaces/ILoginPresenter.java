d3691c8214d86f01772cf4744772e536e2c69b0b
davlatbek <idavlatbek19@gmail.com>
2017-07-11 02:02:07 +0300

    add empty presenter interfaces

---
+++ innopass/src/main/java/innopolis/innopass/interfaces/presenter_interfaces/ILoginPresenter.java
@@ -0,0 +1,9 @@
+package innopolis.innopass.interfaces.presenter_interfaces;
+
+/**
+ * Created by davlet on 7/10/17.
+ */
+
+public interface ILoginPresenter {
+
+}
---
+++ innopass/src/main/java/innopolis/innopass/interfaces/presenter_interfaces/IRegistrationPresenter.java
@@ -0,0 +1,8 @@
+package innopolis.innopass.interfaces.presenter_interfaces;
+
+/**
+ * Created by davlet on 7/10/17.
+ */
+
+public interface IRegistrationPresenter {
+}
